5755783e55d64b99e757aeaec96430bbe01f2d2f
feyzsharaf <feyzullayevsharafx1@protonmail.com>
2026-03-18 00:49:27 +0400

    fix: resolve pdf text overlapping and handle initial data formatting

--- week_2/day_3/assignment_3_1.py
+++ week_2/day_3/assignment_3_1.py
@@ -1,6 +1,6 @@
-    payload = {
-        "model": "google/gemma-3-4b-it:free",  # pulsuz model
-        "messages": [
-            {"role": "user", "content": f"Bu günün NASA APOD təsviri: {apod_description}. Bu hadisənin astronomik əhəmiyyətini 2-3 cümlə ilə izah et."}
-        ]
-    }
+payload = {
+    "model": "google/gemma-3-4b-it:free",  # pulsuz model
+    "messages": [
+        {"role": "user", "content": f"Bu günün NASA APOD təsviri: {apod_description}. Bu hadisənin astronomik əhəmiyyətini 2-3 cümlə ilə izah et."}
+    ]
+}
